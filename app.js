7f947317557fef12ba6e600fc08fc0c9cb936391
santheartist <vazquezbare@gmail.com>
2021-09-06 20:34:33 -0300

    correccion

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const PORT = porcess.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const public = path.resolve(__dirname, "./public");
 app.use(express.static(public));
