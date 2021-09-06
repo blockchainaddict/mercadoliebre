6989df512efe471b145582876b5766a1b82eb0dc
santheartist <vazquezbare@gmail.com>
2021-09-06 20:24:42 -0300

    heroku setup

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const PORT = porcess.env.PORT || 3000;
+
 const public = path.resolve(__dirname, "./public");
 app.use(express.static(public));
 
-app.listen('3030', () => {
-    console.log("Server initialized at http://localhost:3030")
+app.listen(PORT, () => {
+    console.log("Server initialized at http://localhost:3000")
 });
 
 app.get('/', (req,res)=>{
